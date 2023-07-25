a795257805303f29f9da841c9e52d3bea0b7b62d
Siraj Samsudeen <mailsiraj@gmail.com>
2023-07-25 12:20:34 +0530

    styles for the static content

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
       <form>
         <label htmlFor="todo-text">New Item</label>
         <input type="text" name="todo-text" id="todo-text" />
-        <button type="submit">Add</button>
+        <button className="large" type="submit">
+          Add
+        </button>
       </form>
       <h2>Todo Items</h2>
       <ul>
@@ -17,14 +19,14 @@ function App() {
             <input type="checkbox" />
             Todo1
           </label>
-          <button>Delete</button>
+          <button className="delete">Delete</button>
         </li>
         <li>
           <label>
             <input type="checkbox" />
             Todo2
           </label>
-          <button>Delete</button>
+          <button className="delete">Delete</button>
         </li>
       </ul>
     </div>
